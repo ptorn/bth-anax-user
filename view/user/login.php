7bc27b9a6b40dd2b1aa3c620d2c3f536197f2e3a
Peder Tornberg <peder.tornberg@gmail.com>
2017-10-05 09:27:46 +0200

    Fixed minor code uncomments

--- view/user/login.php
+++ view/user/login.php
@@ -5,9 +5,6 @@ namespace Anax\View;
 /**
  * View to create a new comment.
  */
-// Show all incoming variables/functions
-//var_dump(get_defined_functions());
-//echo showEnvironment(get_defined_vars());
 
 // Gather incoming variables and use default values if not set
 $items = isset($items) ? $items : null;
